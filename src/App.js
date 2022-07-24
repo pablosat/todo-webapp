95ceb22ea2e102207505b88d7148eb2d714538bc
psat93 <p.sateler93@gmail.com>
2022-07-24 10:32:19 -0400

    improve addtodo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 export default function App() {
   return (
     <div className="App">
-      <h1>ToDo List app</h1>
+      <h1>Welcome!</h1>
 
       <ToDo />
     </div>
--- src/components/AddToDo/AddToDo.jsx
+++ src/components/AddToDo/AddToDo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { addToDo } from "../toDoReducer";
 
 import "./AddToDo.scss";
@@ -6,26 +6,35 @@ import { checkIfValid } from "./helpers";
 
 const AddToDo = ({ dispatch, state }) => {
   const [toDoName, setToDoName] = useState("");
-  const onChangeHandler = (e) => setToDoName(e.target.value);
-  const handleAddToDo = (e) => {
-    e.preventDefault();
-    const isValid = checkIfValid(state, toDoName);
-    if (isValid) {
-      dispatch(addToDo({ name: toDoName }));
-      setToDoName("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      const isValid = checkIfValid(state, toDoName);
+      if (isValid) {
+        dispatch(addToDo({ name: toDoName }));
+        setToDoName("");
+      }
     }
   };
+
+  const onChangeHandler = (e) => setToDoName(e.target.value);
+
   return (
-    <div>
+    <div className="inputContainer">
       <input
         className="textInput"
         value={toDoName}
         onChange={onChangeHandler}
         placeholder="What do you need to do?"
+        ref={inputRef}
+        onKeyDown={handleKeyDown}
       />
-      <button disabled={!toDoName} onClick={handleAddToDo}>
-        add
-      </button>
     </div>
   );
 };
