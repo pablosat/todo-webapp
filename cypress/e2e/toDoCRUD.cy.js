8b18abba95c820bc0e87b0ea43e63f9e5be42cbe
pablosat <pablo@teu.ai>
2022-08-29 21:08:52 -0400

    add cypress tests

--- cypress/e2e/toDoCRUD.cy.js
+++ cypress/e2e/toDoCRUD.cy.js
@@ -1,14 +1,49 @@
 /// <reference types="cypress" />
+/* eslint no-undef: 0 */
 
-describe("Demo site", () => {
-  it("should add a new ToDo named 'Study react", () => {
-    cy.visit("/");
+const addToDo = (name) => {
+  cy.get("input[data-testid='todoInput']").type(`${name}{enter}`);
+};
+
+const validateExistenceOfToDo = (name, exists = true) => {
+  const shouldActionText = exists ? "exist" : "not.exist";
+  cy.get(`div[data-testid="toDoItem-${name}"]`).should(shouldActionText);
+};
+
+const completeToDo = (name) => {
+  cy.get(`svg[data-testid="complete-${name}"]`).click();
+};
+
+beforeEach(() => {
+  cy.visit("/");
+});
+
+describe("To Do CRUD functions", () => {
+  it("should add a new ToDo named 'Study react'", () => {
     const toDoName = "Study react";
-    cy.get("input[data-testid=\"todoInput\"]").type(`${toDoName}{enter}`);
-    const asd = cy
-      .get(`div[data-testid="toDoItem-${toDoName}"]`)
-      .first()
-      .should("have.text", toDoName);
-    console.log({ asd });
+    addToDo(toDoName);
+    validateExistenceOfToDo(toDoName);
+  });
+  it("should mark as completed the ToDo named 'Study react'", () => {
+    const toDoName = "Study react";
+    addToDo("Study nodejs");
+    addToDo(toDoName);
+    completeToDo(toDoName);
+    cy.get(`div[data-testid="toDoItem-${toDoName}"]`).should("have.class", "item completed");
+  });
+  it("should change the name of the 'Study react' ToDo to 'Study js", () => {
+    const toDoName = "Study react";
+    const newName = "Study js";
+    addToDo("Study nodejs");
+    addToDo(toDoName);
+    cy.get(`svg[data-testid="edit-${toDoName}"]`).type(`{selectAll}{backspace}${newName}{enter}`);
+    validateExistenceOfToDo(newName);
+  });
+  it("should delete the todo named 'Study nodejs'", () => {
+    const toDoName = "Study nodejs";
+    addToDo("Study react");
+    addToDo(toDoName);
+    cy.get(`svg[data-testid="delete-${toDoName}"]`).click();
+    validateExistenceOfToDo(toDoName, false);
   });
 });
