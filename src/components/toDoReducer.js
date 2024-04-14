8c3bbecb55f0c99cec626063c3db0de2270b9dbe
Pablo Sateler <p.sateler93@gmail.com>
2024-04-14 13:51:47 -0400

    new lists

--- src/components/toDoReducer.js
+++ src/components/toDoReducer.js
@@ -16,7 +16,63 @@ export const toDoReducer = (state, action = { type: "", payload: {} }) => {
     case editToDo().type:
       return state.map((todo) => (todo.id === payload.id ? { ...todo, name: payload.name } : todo));
     default:
-      return [];
+      return [
+        {
+          id: 2,
+          name: "Comprar Tele",
+          completed: true
+        },
+        {
+          id: 6,
+          name: "Comprar Rack",
+          completed: true
+        },
+        {
+          id: 7,
+          name: "Hacer menu",
+          completed: true
+        },
+        {
+          id: 3,
+          name: "Botar ropa",
+          completed: false
+        },
+        {
+          id: 123,
+          name: "Devolver mochilas alicia",
+          completed: false
+        },
+        {
+          id: 4,
+          name: "Botar cosas bodega",
+          completed: false
+        },
+        {
+          id: 1,
+          name: "Definir plan de inversion",
+          completed: false
+        }
+        // {
+        //   id: 5,
+        //   name: "Playlist",
+        //   completed: false
+        // },
+        // {
+        //   id: 8,
+        //   name: "Definir mesas",
+        //   completed: false
+        // },
+        // {
+        //   id: 10,
+        //   name: "Zapatos",
+        //   completed: false
+        // },
+        // {
+        //   id: 11,
+        //   name: "Corbata",
+        //   completed: false
+        // }
+      ];
   }
 };
 
